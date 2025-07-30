e581d9b0fb7f065dd4311b2dd4342b8510f6a0f8
rafaelrdacosta <rafaelrdacosta@gmail.com>
2025-07-29 20:50:58 -0400

    Estudo 29 JUL 25

---
+++ Cap 9/prog0904.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+main(){
+    char Nome_In[100], Nome_Out[100];
+
+    printf("Nome do arquivo de entrada: ");
+    gets(Nome_In);
+    printf("Nome do arquivo de saida: ");
+    gets(Nome_Out);
+}
